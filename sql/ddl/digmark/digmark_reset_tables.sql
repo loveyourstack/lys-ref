44cfa07f42c96304bca82927ac734b670dfae77d
LoveYourStack <loveyourstack@gmail.com>
2026-08-17 11:14:53 +0200

    reset gen camps

--- sql/ddl/digmark/digmark_reset_tables.sql
+++ sql/ddl/digmark/digmark_reset_tables.sql
@@ -2,6 +2,7 @@
 TRUNCATE TABLE digmark.campaign_performance_aggregated;
 TRUNCATE TABLE digmark.campaign_performance;
 DELETE FROM digmark.campaign;
+DELETE FROM digmark.generated_campaign;
 DELETE FROM digmark.launcher_fb;
 DELETE FROM digmark.launcher_gads;
 DELETE FROM digmark.vertical;
